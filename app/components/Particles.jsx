7ce63530e0070ba8ed005b7bb340b3f5d4e83217
Guardian Prodigy <hahdhdhwj@gmail.com>
2023-07-09 11:13:32 +0200

    Deployed Store

---
+++ app/components/Particles.jsx
@@ -0,0 +1,146 @@
+import React, {useState, useCallback, useEffect} from 'react';
+import Particles from 'react-tsparticles';
+import {loadFull} from 'tsparticles';
+
+export const ParticlesContainer = () => {
+  const [loading, setLoading] = useState(true);
+
+  const particlesInit = useCallback(async (particles) => {
+    console.log(particles);
+    await loadFull(particles);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container);
+  }, []);
+  useEffect(() => {
+    setLoading(false);
+  }, [loadFull]);
+  if (loading) {
+    return null; // Return null or a loading indicator while particles are loading
+  }
+
+  return (
+    <Particles
+      id="tsparticle"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -999,
+        },
+        particles: {
+          number: {
+            value: 10,
+            density: {
+              enable: false,
+              value_area: 500,
+            },
+          },
+          color: {
+            value: ['#0362c1', '#D4AF37'],
+          },
+          shape: {
+            type: 'star',
+            options: {
+              sides: 50,
+            },
+          },
+          opacity: {
+            value: 1,
+            random: false,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.9,
+              sync: true,
+            },
+          },
+          size: {
+            value: 3,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.9,
+              sync: false,
+            },
+          },
+          rotate: {
+            value: 0,
+            random: true,
+            direction: 'clockwise',
+            animation: {
+              enable: true,
+              speed: 5,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: {
+              value: ['#ffffffef'],
+            },
+            opacity: 0.5,
+            width: 2,
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            attract: {
+              enable: true,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: ['grab'],
+            },
+            onclick: {
+              enable: false,
+              mode: 'bubble',
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 200,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 6,
+              duration: 2,
+              opacity: 8,
+              speed: 3,
+            },
+            repulse: {
+              distance: 100,
+              duration: 2,
+            },
+          },
+        },
+        retina_detect: true,
+        background: {
+          color: '#111',
+          image: '',
+          position: '50% 50%',
+          repeat: 'no-repeat',
+          size: 'cover',
+        },
+      }}
+    />
+  );
+};
---
+++ app/components/ProductGallery.jsx
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import {Image} from '@shopify/hydrogen';
+
+/**
+ * A client component that defines a media gallery for hosting images, 3D models, and videos of products
+ */
+export function ProductGallery({media, className}) {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (media.length > 0) {
+      setSelectedImage(media[0]);
+    }
+  }, [media]);
+
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  if (!media.length) {
+    return null;
+  }
+
+  return (
+    <div className={className}>
+      <div className="main-image-container">
+        {selectedImage && selectedImage.__typename === 'MediaImage' && (
+          <Image
+            data={selectedImage.image}
+            alt={selectedImage.alt || 'Product image'}
+            className={`object-cover w-full h-full aspect-square fadeIn main-image ${
+              isLoading ? 'skeleton' : ''
+            }`}
+            onLoad={handleImageLoad}
+          />
+        )}
+        {isLoading && <div className="skeleton-animation" />}
+      </div>
+      <div className="thumbnail-carousel">
+        <div className="thumbnails">
+          {media.map((med, i) => {
+            const image = med.__typename === 'MediaImage' ? med.image : null;
+
+            const altText =
+              med.__typename === 'MediaImage'
+                ? med.alt || 'Product image'
+                : 'Product image';
+
+            return (
+              <div
+                key={med.id || image?.id}
+                onClick={() => handleThumbnailClick(med)}
+                className={`thumbnail ${selectedImage === med ? 'active' : ''}`}
+              >
+                {image && (
+                  <Image
+                    data={image}
+                    alt={altText}
+                    className={`thumbnail-image ${isLoading ? 'skeleton' : ''}`}
+                    onLoad={handleImageLoad}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
